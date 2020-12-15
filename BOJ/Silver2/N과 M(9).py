10361241bdfbe5517111754baec108903f2f9b97
tjdgus3160 <tjdgus3160@gmail.com>
2020-12-15 22:17:58 +0900

    12.15

---
+++ BOJ/Silver2/N과 M(9).py
@@ -0,0 +1,22 @@
+# 15663번
+from collections import Counter
+import sys
+input=sys.stdin.readline
+
+def func(k):
+    if k==m:
+        print(*arr[:-1])
+        return
+    for i in isused:
+        if isused[i]!=0:
+            arr[k]=i
+            isused[i]-=1
+            func(k+1)
+            isused[i]+=1
+
+n,m=map(int,input().split())
+num=[*map(int,input().split())]
+num.sort()
+arr=[0]*(m+1)
+isused=Counter(num)
+func(0)
